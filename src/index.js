b0be15354ba1eae3e0a47c3521d8759b1ed70d93
Anastasiia Haliara <an.haliara@gmail.com>
2022-08-14 18:46:09 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,80 @@
 import './css/styles.css';
+import {fetchCountries} from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+searchInput.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY))
+
+function onSearchInput(e) {
+    e.preventDefault();
+
+    const countryName = searchInput.value.trim();
+
+    if (countryName === "") {
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+        return;
+    }
+    
+    fetchCountries(countryName)
+        .then(countries => {
+            countryList.innerHTML = '';
+            countryInfo.innerHTML = '';
+            
+            if (countries.length === 1) {
+                countryList.insertAdjacentHTML('beforeend', renderCountryList(countries));
+                countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries));
+            }
+            else if (countries.length > 10) {
+                alertTooManyMatches();
+            } else {
+                countryList.insertAdjacentHTML('beforeend', renderCountryList(countries));
+            }
+        })
+        .catch(alertWrongName);
+
+}
+
+function renderCountryList(countries) {
+    const listMarkup = countries
+        .map(({ name, flags }) => {
+            return `
+        <li class="country-list__item">
+            <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 25px height = 25px>
+            <h2 class="country-list__name">${name.official}</h2>
+        </li>
+        `
+        })
+        .join('');
+    return listMarkup;
+}
+
+function renderCountryInfo(countries) {
+    const infoMarkup = countries
+        .map(({ capital, population, languages }) => {
+            return `
+        <ul class="country-info__list">
+            <li class="country-info__item"><span class="country-info__item--weight">Capital:</span> ${capital}</li>
+            <li class="country-info__item"><span class="country-info__item--weight">Population:</span> ${population}</li>
+            <li class="country-info__item"><span class="country-info__item--weight">Languages:</span> ${Object.values(languages)}</li>
+        </ul>
+        `
+        })
+        .join('');
+    return infoMarkup;
+}
+
+
+function alertWrongName() {
+    Notify.failure('Oops, there is no country with that name');
+}
+
+function alertTooManyMatches() {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/'
+const searchOptions = 'fields=name,capital,population,flags,languages'
+
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}?${searchOptions}`)
+    .then(response => response.json())
+    .catch(error => console.log(error))
+}
